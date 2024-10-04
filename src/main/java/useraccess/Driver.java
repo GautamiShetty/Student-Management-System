057cdda729b6c4a1d5de0a61c085c385dfeae084
GautamiShetty <95223703+GautamiShetty@users.noreply.github.com>
2024-10-04 11:24:32 +0530

    Add files via upload

---
+++ src/main/java/useraccess/Driver.java
@@ -0,0 +1,9 @@
+package useraccess;
+import management.login.LoginPage;
+
+public class Driver {
+	public static void main(String[] args) {
+		LoginPage login = new LoginPage();
+		login.loginPage();
+	}
+}
